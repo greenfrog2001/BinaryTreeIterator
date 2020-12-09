26bab6f56a26f33d2c5a4a80b629cbe76a424417
greenfrog2001 <53978309+greenfrog2001@users.noreply.github.com>
2020-12-10 03:19:50 +0700

    Shift changes made to new class
    Remove all changes made on the original preOrderIterator class and copy that changes to a new class call postOrderIterator

--- LinkedTree.java
+++ LinkedTree.java
@@ -342,6 +342,59 @@ public class LinkedTree {
         private PreorderIterator() {
             // The traversal starts with the root node.
             nextNode = root;
+        }
+        
+        public boolean hasNext() {
+            return (nextNode != null);
+        }
+        
+        public int next() {
+            if (nextNode == null) {
+                throw new NoSuchElementException();
+            }
+            
+            // Store a copy of the key to be returned.
+            int key = nextNode.key;
+            
+            // Advance nextNode.
+            if (nextNode.left != null) {
+                nextNode = nextNode.left;
+            } else if (nextNode.right != null) {
+                nextNode = nextNode.right;
+            } else {
+                // We've just visited a leaf node.
+                // Go back up the tree until we find a node
+                // with a right child that we haven't seen yet.
+                Node parent = nextNode.parent;
+                Node child = nextNode;
+                while (parent != null &&
+                       (parent.right == child || parent.right == null)) {
+                    child = parent;
+                    parent = parent.parent;
+                }
+                
+                if (parent == null) {
+                    nextNode = null;  // the traversal is complete
+                } else {
+                    nextNode = parent.right;
+                }
+            }
+            
+            return key;
+        }
+    }
+    
+    /* Returns a postorder iterator for this tree. */
+    public LinkedTreeIterator postorderIterator() {
+        return new postorderIterator();
+    }
+    
+    /*inner class for a preorder iterator */
+    private class PostorderIterator implements LinkedTreeIterator {
+        private Node nextNode;
+        
+        private PostorderIterator() {
+            nextNode = root;
             while (nextNode.left != null) {
                 nextNode = nextNode.left;   
             }
